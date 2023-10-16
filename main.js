7836a81208f3e60a142f192adb60a1be4d4ca321
justacurlygirl <99064129+Akshayavelan@users.noreply.github.com>
2023-10-16 14:16:36 +0530

    main.js

--- main.js
+++ main.js
@@ -12,30 +12,4 @@ let password;
         }
     };
  
-  //Sign Up credentials
-  let firstname;
-  let lastname;;
-  let phone;
-  let email;
-  let confirmp;
-  let create;
-
-    document.getElementById("box1").onclick = function(){
-        firstname = document.getElementById("fname").value;
-        lastname = document.getElementById("lname").value;
-        console.log(fname+lname);
-        phone = document.getElementById("phone").value;
-        email = document.getElementById("email").value;
-        create = document.getElementById("cpassword").value;
-        confirmp= document.getElementById("conpassword").value;
-        if(confirmp==create){
-            console.log("password set");
-        }
-
-        if(firstname && lastname && phone && email && confirmp && create){
-            console.log("Welcome to DreamCar");
-        }else{
-            console.log("fill up all fields");
-        }
-    };  
-    
+  
